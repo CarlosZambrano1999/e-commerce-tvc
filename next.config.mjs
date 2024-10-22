20ef441e8ab9db361d31ee65207869f20600429d
Carlos Daniel Zambrano <cdanzamcol@gmail.com>
2024-10-21 23:26:50 -0600

    Creación del proyecto
    Documentos principales

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+  images: {
+    domains: ['www.elgallomasgallo.com.hn'], // Agrega el dominio aquí
+  },
+};
+
+export default nextConfig;
