935286de68dc3017042b8bcba32cb2119086cfab
Carlos Daniel Zambrano <cdanzamcol@gmail.com>
2024-10-22 01:13:55 -0600

    Elementos básicos
    Creación del navbar, interfaces necesarias y Page not Found

---
+++ src/components/ui/top-menu/TopMenu.tsx
@@ -0,0 +1,66 @@
+'use client';
+import { useUIStore } from "@/store/ui/ui-store"
+import Link from "next/link"
+import {IoSearchOutline, IoCartOutline, IoPersonCircleOutline} from 'react-icons/io5'
+import Image from 'next/image';
+
+
+export const TopMenu = () =>{
+    
+    const openSideMenu = useUIStore( state=>state.openSideMenu)
+    return (
+        <nav className="flex px-5 justify-between items-center w-full">
+            {/*Logo*/ }
+            <div>
+                <Link
+                 href="/">
+                        <Image
+                        src={"/imgs/logo-tvc.png"}
+                        alt="TVC"
+                        className="absolute mt-1 mr-10 "
+                        width={30}
+                        height={30} />
+                        <span className="ml-10"> SHOP</span>
+                </Link>
+            </div>
+
+            {/*Center Menú */}
+            <div className="hidden sm:block">
+                <Link className="m-2 p-2 rounded-md transition-all hover:bg-gray-100" href="/category/hogar">
+                Hogar
+                </Link>
+                <Link className="m-2 p-2 rounded-md transition-all hover:bg-gray-100" href="/category/jardineria">
+                Jardinería
+                </Link>
+                <Link className="m-2 p-2 rounded-md transition-all hover:bg-gray-100" href="/category/electrodomesticos">
+                Electrodomésticos
+                </Link>
+            </div>
+
+            {/*Search, Cart, Menu */}
+            <div className="flex items-center">
+                <Link href="/search" className="mx-2 hidden sm:block">
+                    <IoSearchOutline className="w-5 h-5 absolute top-5 ml-2" />
+                    <input 
+                type='text'
+                placeholder="Buscar Productos"
+                className="w-full bg-gray-50 rounded-full pl-10 py-1 pr-10 border-b-2 text-xl border-gray-200 focus:outline-none focus:border-blue-500" />
+                </Link>
+                <Link href="/cart">
+                    <div className="relative">
+                    <span className="absolute text-xs rounded-full px-1  font-bold -top-2 -right-2 bg-blue-700 text-white .rigth-2">
+                        3
+                    </span>
+                        <IoCartOutline className="w-5 h-5" />
+                    </div>
+                </Link>
+
+                <button
+                onClick={openSideMenu}
+                className="m-2 p-2 rounded-md transition-all hover:bg-gray-100">
+                    <IoPersonCircleOutline className="w-5 h-5" />
+                </button>
+            </div>
+        </nav>
+    )
+}
